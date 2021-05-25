46afdd42ff1bbbf8cd9d74fda7cf2f430b5415db
Vietta-hhanh <74322475+Vietta-hhanh@users.noreply.github.com>
2021-05-25 19:35:21 +0300

    day00
    asd

---
+++ day00/ex00/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+Main(args);
+
+        int Main(string[] args)
+        {
+			if (args.Length != 5)
+			{
+    			System.Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+				return (1);
+
+			}
+			double sum;
+			double rate;
+			int term;
+			int selectedMonth;
+			double payment;
+			bool test;
+
+			test = double.TryParse(args [0], out sum);
+			if (test == false)
+			{
+				System.Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+				return (1);
+			}
+
+			test = double.TryParse(args [1], out rate);
+			if (test == false)
+			{
+				System.Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+				return (1);
+			}
+
+			test = int.TryParse(args [2], out term);
+			if (test == false)
+			{
+				System.Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+				return (1);
+			}
+
+			test = int.TryParse(args [3], out selectedMonth);
+			if (test == false)
+			{
+				System.Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+				return (1);
+			}
+
+			test = double.TryParse(args [4], out payment);
+			if (test == false)
+			{
+				System.Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+				return (1);
+			}
+
+			double mounthrate = rate / 12 / 100;
+			int daysInYear = (new System.DateTime(2022, 1, 1) - new System.DateTime(2021, 1 ,1 )).Days;
+			int Mounth = 4;
+			int StartMounth = 5;
+			int countMounth;
+			int year = 2021;
+			int yearlow = 2021;
+			double pay;
+			double newsum = sum;
+			System.TimeSpan subtractdates;
+			double procent;
+			double res = 0;
+			countMounth = 0;
+
+		
+			while (newsum >= 0)
+			{
+				if (Mounth + 1 - StartMounth == selectedMonth)
+					newsum = newsum - payment;
+				if (Mounth + 1 - StartMounth >= selectedMonth)
+					pay = (newsum * mounthrate * System.Math.Pow((1 + mounthrate), term - countMounth)) / (System.Math.Pow(1 + mounthrate, term - countMounth) - 1);
+				else
+					pay = (newsum * mounthrate * System.Math.Pow((1 + mounthrate), term - countMounth)) / (System.Math.Pow(1 + mounthrate, term - countMounth) - 1);
+				countMounth++;
+				if (Mounth % 11 == 0)
+					year += 1;
+				subtractdates = new System.DateTime(year, (((Mounth + 1) % 12) + 1), 1) - new System.DateTime(yearlow, (Mounth % 12) + 1, 1);
+				Mounth += 1;
+				if (Mounth > 12)
+				Mounth = 1;
+				if (year != yearlow)
+					yearlow += 1; 
+				procent = newsum * rate * subtractdates.Days / (daysInYear * 100);
+				res = res + procent;
+				newsum = newsum - pay + procent;
+			}
+			double res1 = res;
+
+			//  РАСЧЕТ 2!
+
+			Mounth = 4;
+			StartMounth = 5;
+			year = 2021;
+			yearlow = 2021;
+			res = 0;
+			newsum = sum;
+			pay = (sum * mounthrate * System.Math.Pow((1 + mounthrate), term )) / (System.Math.Pow(1 + mounthrate, term ) - 1);
+			while (newsum >= 0)
+			{
+				if (Mounth + 1 - StartMounth == selectedMonth)
+					newsum = newsum - payment;
+				if (Mounth % 11 == 0)
+				year += 1;
+	
+				subtractdates = new System.DateTime(year, (((Mounth + 1) % 12) + 1), 1) - new System.DateTime(yearlow, (Mounth % 12) + 1, 1);
+				Mounth += 1;
+				if (Mounth > 12)
+				Mounth = 1;
+				if (year != yearlow)
+					yearlow += 1; 
+				procent = newsum * rate * subtractdates.Days / (daysInYear * 100);
+				res = res + procent;
+				newsum = newsum - pay + procent;
+			}
+
+			// ВЫВОД РЕЗУЛЬТАТА
+			double res2 = res;
+			res = res2 - res1;
+			if (res == 0)
+				Console.WriteLine("Переплата при уменьшении платежа: " + res1 + "р.\nПереплата при уменьшении срока: " + res2 + "р.\nПереплата одинакова в обоих вариантах.");
+			if (res > 0)
+				Console.WriteLine("Переплата при уменьшении платежа: " + res1 + "р.\nПереплата при уменьшении срока: " + res2 + "р.\nУменьшение платежа выгоднее уменьшения срока на "+res+"р.");
+			if (res < 0)
+				Console.WriteLine("Переплата при уменьшении платежа: " + res1 + "р.\nПереплата при уменьшении срока: " + res2 + "р.\nУменьшение срока выгоднее уменьшения платежа на "+ (-res)+ "р.");
+			return (0);
+        }
---
+++ day00/ex01/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+Main(args);
+
+        int Main(string[] args)
+        {
+			string path = "./us.txt";
+			if (!File.Exists(path))
+			{
+				Console.WriteLine("Ошибка, файла us.txt не существует");
+				return (1);
+			}
+			Console.WriteLine("Enter name:");
+			string Name = Console.ReadLine();
+			if (Name == "")
+			{
+				Console.WriteLine("Имя не введено");
+				return (2);
+			}
+			int check = 0;
+			while (check < Name.Length)
+			{
+				if (Name[check] == ' ' || Name[check] == '-' )
+				{
+					Console.WriteLine("Wrong name");
+					return (1);
+				}
+				check++;
+			}
+			int lenName;
+			int lenEdit;
+			string[] ListOfNames = File.ReadAllLines(path);
+			foreach (string edit in  ListOfNames)
+			{
+				lenName = Name.Length;
+				lenEdit = edit.Length;
+			
+				int [,] array = new int[lenName + 1, lenEdit + 1];
+				int i, j;
+				i = 0;
+				j = 0;
+				while (i < lenName + 1)
+				{
+					j = 0;
+					while(j < lenEdit + 1)
+					{
+						array[i,j] = 0;
+						j++;
+					}
+					i++;
+				}
+				i = 1;
+				while (i < lenEdit + 1)
+				{
+					array[0, i] = i;
+					i++;
+				}
+				i = 1;
+				while (i < lenName + 1)
+				{
+					array[i, 0] = i;
+					i++;
+				}
+				i = 1;
+				
+				int minimal;
+				while (i < lenName + 1)
+				{
+					j = 1;
+					while (j < lenEdit + 1)
+					{
+						if (Name[i - 1] == edit[j - 1])
+							array[i,j] = array[i - 1, j - 1];
+						else
+						{
+							minimal = array[i - 1, j - 1];
+							if (array[i - 1, j] < minimal)
+							minimal = array[i - 1, j];
+							if (array[i, j - 1] < minimal)
+							minimal = array[i, j - 1];
+							array[i,j] = minimal + 1;
+						}
+						j++;
+					}
+					i++;
+				}
+				int result = array[i - 1, j - 1];
+				if (result < 3)
+				{
+					Console.WriteLine("Did you mean " + edit + "? Y/N");
+					string answer = Console.ReadLine();
+					if ( answer == "Y" || answer == "y")
+					{
+						Console.WriteLine("Hello, "+ edit +"!");
+						return 0;
+					}
+					else if (answer == "N" || answer == "n")
+					{
+					}
+					else 
+					{
+						Console.WriteLine ("Имя не введено");
+						return (1);
+					}
+				}
+
+
+
+
+			}
+			Console.WriteLine("Your name was not found.");
+			return (0);
+			
+        }
